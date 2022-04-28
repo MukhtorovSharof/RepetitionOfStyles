993af4486579e9b0d8ea97bfa4b0143c4e1e3cce
Mukhtorov <sharofmuxtorov979@gmail.com>
2022-04-28 20:34:58 +0500

    repetition of styles

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+  {
+    productName: "Foldable Mini Drone",
+    productNumber: 85631,
+    paymentStatus: "Zue",
+    shipping: "Pending"
+  },
+  {
+    productName: "Foldable Clone Drone",
+    productNumber: 85631,
+    paymentStatus: "Mue",
+    shipping: "Declined"
+  },
+  {
+    productName: "Foldable Great Drone",
+    productNumber: 85631,
+    paymentStatus: "Due",
+    shipping: "Pending"
+  },
+  {
+    productName: "Foldable Big Drone",
+    productNumber: 85631,
+    paymentStatus: "Rue",
+    shipping: "Delivered"
+  },
+  {
+    productName: "Foldable Better Drone",
+    productNumber: 85631,
+    paymentStatus: "Vue",
+    shipping: "Delivered"
+  },
+  {
+    productName: "Foldable Good Drone",
+    productNumber: 85631,
+    paymentStatus: "Bue",
+    shipping: "Pending"
+  },
+  {
+    productName: "Foldable Crazy Drone",
+    productNumber: 85631,
+    paymentStatus: "Kue",
+    shipping: "Delivered"
+  }
+]
